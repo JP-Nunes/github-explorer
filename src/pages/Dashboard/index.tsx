e622d1a4f14e3f21b6c8046a4fba7054161435d0
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-19 17:35:45 -0300

    Implementing dashboard's interface

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Dashboard</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+          <img
+            src="https://avatars1.githubusercontent.com/u/51239826?s=460&u=aa1e806fc6a256c7e607c3b9b889c8e656e8da87&v=4"
+            alt="User Image"
+          />
+          <div>
+            <strong>github-explorer</strong>
+            <p>Easy peasy way to explore github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard
