3f950b6720c65edc4220ab8822bb1c1e729f08e5
João Pedro Nunes <joao.p_nunes@outlook.com>
2020-06-20 15:27:12 -0300

    Dealing with errors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,20 +3,21 @@ import { FiChevronRight } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
 
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
 import api from '../../services/api'
 
 interface Repository {
   full_name: string
   description: string
   owner: {
-    login: string,
+    login: string
     avatar_url: string
   }
 }
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('')
+  const [inputError, setInputError] = useState('')
   const [repositories, setRepositories] = useState<Repository[]>([])
 
   async function handleAddRepository(
@@ -24,30 +25,40 @@ const Dashboard: React.FC = () => {
   ): Promise<void> {
     event.preventDefault()
 
-    const response = await api.get<Repository>(`repos/${newRepo}`)
-    const repository = response.data
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório')
+      return
+    }
 
-    setRepositories([...repositories, repository])
-    setNewRepo('')
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
+      const repository = response.data
+
+      setRepositories([...repositories, repository])
+      setNewRepo('')
+    } catch (err) {
+      setInputError('Erro ao buscar repositório')
+    }
   }
 
   return (
     <>
-      <img src={logoImg} alt="Github Explorer"/>
+      <img src={logoImg} alt="Github Explorer" />
       <Title>Dashboard</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
           placeholder="Digite o nome do repositório"
-
         />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        {repositories.map(repository => (
+        {repositories.map((repository) => (
           <a key={repository.full_name} href="#">
             <img
               src={repository.owner.avatar_url}
